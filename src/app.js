f92f5f3cd90439c6a40d7d0036daedeb9683b92f
Deepika Bondu <deepikabondu04@gmail.com>
2024-09-21 18:27:10 +0530

    about text added

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ const { error } = require('console')
 
 // define paths for express configure
 const app = express()
+const port = process.env.PORT || 3000
+
 const pubilcDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -114,6 +116,6 @@ app.get('*',(req,res)=>{
 
 
 
-app.listen(3000, ()=>{
-    console.log('server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
